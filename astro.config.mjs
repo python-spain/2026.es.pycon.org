e946a10f01f65d8d1c8a6d9c27908d0ad323a166
itziarZG <itziar.zameza.dev@gmail.com>
2026-01-11 19:14:40 +0100

    fix: fix config to dns

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
+// CONF WITHOUT REAL DOMAIN
 export default defineConfig({
   site: 'https://python-spain.github.io',
   base: '/2026.es.pycon.org',
 });
+
+//CONFIG WITH REAL DOMAIN
+// astro.config.mjs
+// export default defineConfig({
+//   site: 'https://2026.es.pycon.org',
+//   base: '/',
+// });

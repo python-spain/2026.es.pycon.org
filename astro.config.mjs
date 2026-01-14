29d5332424e2f268ee20ff5d5210dfb964475a2e
Francesc Arpi Roca <francesc.arpi@gmail.com>
2026-01-14 18:54:17 +0100

    feat: first landing with essential info

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 // CONF WITHOUT REAL DOMAIN
 export default defineConfig({
   site: 'https://python-spain.github.io',
   base: '/2026.es.pycon.org',
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
 
 //CONFIG WITH REAL DOMAIN

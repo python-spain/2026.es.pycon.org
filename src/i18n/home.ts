9fe8c13868606da3aa7f835cee707342bc699bf4
Francesc Arpi Roca <francesc.arpi@gmail.com>
2026-01-30 17:19:48 +0100

    refactor: language picker improvements
    - Remove unnecessary code
    - Add styles to language picker
    - Replace copy sponsors email by a link to sponsors
    - Apply prettier

--- src/i18n/home.ts
+++ src/i18n/home.ts
@@ -3,21 +3,15 @@ export const texts = {
     'index.initializing': 'Inicializando sistema...',
     'index.subtitle': 'Sede UB Barcelona | 6-8 Nov 2026',
     'index.sponsor_btn': 'PATROCINA',
-    'index.copy_hint': 'Copia el email de contacto',
-    'index.copied': '¡EMAIL COPIADO!',
   },
   en: {
     'index.initializing': 'Initialising system...',
     'index.subtitle': 'UB Barcelona Venue | Nov 6-8, 2026',
     'index.sponsor_btn': 'BECOME A SPONSOR',
-    'index.copy_hint': 'Copy contact email',
-    'index.copied': 'EMAIL COPIED!',
   },
   ca: {
     'index.initializing': 'Inicialitzant sistema...',
     'index.subtitle': 'Seu UB Barcelona | 6-8 Nov 2026',
     'index.sponsor_btn': 'PATROCINA',
-    'index.copy_hint': "Copia l'email de contacte",
-    'index.copied': 'EMAIL COPIAT!',
   },
 } as const
--- src/pages/constants.ts
+++ src/pages/constants.ts
@@ -1,2 +1,1 @@
 export const SPONSORS_EMAIL = 'sponsors@2026.es.pycon.org'
-export const SPONSORS_SUBJECT = 'Interés de patrocinio para PyConES2026'

1ceb02e2cf0565711d75df4b70f9d026888c60ff
Francesc Arpi Roca <francesc.arpi@gmail.com>
2026-02-17 08:31:11 +0100

    refactor: transforma home en secciones y añade un mensaje

--- src/i18n/home.ts
+++ src/i18n/home.ts
@@ -2,16 +2,19 @@ export const texts = {
   es: {
     'index.initializing': 'Inicializando sistema...',
     'index.subtitle': 'Sede UB Barcelona | 6-8 Nov 2026',
-    'index.sponsor_btn': 'PATROCINA',
+    'index.message':
+      'Forma parte de la mayor conferencia nacional de Python, donde cientos de entusiastas, profesionales y empresas se reúnen para compartir conocimientos, experiencias y oportunidades en el mundo de Python. ¡No te pierdas esta oportunidad única de aprender, conectar y crecer en la comunidad Python!',
   },
   en: {
     'index.initializing': 'Initialising system...',
     'index.subtitle': 'UB Barcelona Venue | Nov 6-8, 2026',
-    'index.sponsor_btn': 'BECOME A SPONSOR',
+    'index.message':
+      "Join the largest national Python conference, where hundreds of enthusiasts, professionals, and companies come together to share knowledge, experiences, and opportunities in the world of Python. Don't miss this unique opportunity to learn, connect, and grow in the Python community!",
   },
   ca: {
     'index.initializing': 'Inicialitzant sistema...',
     'index.subtitle': 'Seu UB Barcelona | 6-8 Nov 2026',
-    'index.sponsor_btn': 'PATROCINA',
+    'index.message':
+      'Forma part de la major conferència nacional de Python, on centenars d’entusiastes, professionals i empreses es reuneixen per compartir coneixements, experiències i oportunitats en el món de Python. No et perdis aquesta oportunitat única d’aprendre, connectar i créixer en la comunitat Python!',
   },
 } as const

7953be5c0b28025425364fa2332bb45bfcc8af60
Francesc Arpi Roca <francesc.arpi@gmail.com>
2026-02-24 08:52:37 +0100

    feat: add sponsors description
    Also add a TextBox generic component

--- src/i18n/home.ts
+++ src/i18n/home.ts
@@ -4,17 +4,26 @@ export const texts = {
     'index.subtitle': 'Sede UB Barcelona | 6-8 Nov 2026',
     'index.message':
       'Forma parte de la mayor conferencia nacional de Python, donde cientos de entusiastas, profesionales y empresas se reúnen para compartir conocimientos, experiencias y oportunidades en el mundo de Python. ¡No te pierdas esta oportunidad única de aprender, conectar y crecer en la comunidad Python!',
+    'sponsors.title': 'Patrocinios',
+    'sponsors.description':
+      'Gracias a las empresas que colaboran con la PyConES podemos ofrecer el mejor evento y experiencia posible. Somos una conferencia con un bajo coste de entrada capaz de ofrecer una experiencia de 3 días incluyendo regalos, almuerzos, comidas y meriendas. Además contamos con servicio de guardería, becas y traducción en directo a lenguaje de signos para que nadie se quede fuera. Con la ayuda de estas empresas conseguimos hacer un evento diverso e inclusivo enfocado en cuidar la comunidad de Python.',
   },
   en: {
     'index.initializing': 'Initialising system...',
     'index.subtitle': 'UB Barcelona Venue | Nov 6-8, 2026',
     'index.message':
       "Join the largest national Python conference, where hundreds of enthusiasts, professionals, and companies come together to share knowledge, experiences, and opportunities in the world of Python. Don't miss this unique opportunity to learn, connect, and grow in the Python community!",
+    'sponsors.title': 'Sponsorships',
+    'sponsors.description':
+      'Thanks to the companies that collaborate with PyConES, we can offer the best possible event and experience. We are a conference with a low entry cost capable of offering a 3-day experience including gifts, lunches, meals, and snacks. Additionally, we have childcare services, scholarships, and live sign language translation to ensure that no one is left out. With the help of these companies, we manage to create a diverse and inclusive event focused on caring for the Python community.',
   },
   ca: {
     'index.initializing': 'Inicialitzant sistema...',
     'index.subtitle': 'Seu UB Barcelona | 6-8 Nov 2026',
     'index.message':
       'Forma part de la major conferència nacional de Python, on centenars d’entusiastes, professionals i empreses es reuneixen per compartir coneixements, experiències i oportunitats en el món de Python. No et perdis aquesta oportunitat única d’aprendre, connectar i créixer en la comunitat Python!',
+    'sponsors.title': 'Patrocinis',
+    'sponsors.description':
+      'Gràcies a les empreses que col·laboren amb la PyConES podem oferir el millor esdeveniment i experiència possible. Som una conferència amb un baix cost d’entrada capaç d’oferir una experiència de 3 dies incloent regals, àpats, menjars i berenars. A més comptem amb servei de guarderia, beques i traducció en directe a llenguatge de signes perquè ningú es quedi fora. Amb l’ajuda d’aquestes empreses aconseguim fer un esdeveniment divers i inclusiu enfocat en cuidar la comunitat de Python.',
   },
 } as const

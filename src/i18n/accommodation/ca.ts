312f9f6ca256b5da026be5e606103ac9417753c9
itziarZG <itziar.zameza.dev@gmail.com>
2026-03-17 18:46:11 +0100

    feat:mpve trans to folder

---
+++ src/i18n/accommodation/ca.ts
@@ -0,0 +1,25 @@
+export const ca = {
+  'accommodation.title': 'Allotjament',
+  'accommodation.hero.title': 'On allotjar-se',
+  'accommodation.hero.subtitle': 'Recomanacions per a la teva estada a Barcelona',
+  'accommodation.intro':
+    "Barcelona és una de les ciutats amb més oferta hotelera d'Europa. Per a la PyConES 2026, recomanem buscar allotjament al districte de l'Eixample o a Ciutat Vella per estar a pocs minuts a peu de la seu (UB Edifici Històric).",
+  'accommodation.hotels.title': 'Hotels Recomanats',
+  'accommodation.hotels.subtitle': 'Convenis i opcions properes',
+  'accommodation.hotels.disclaimer':
+    'Estem treballant per tancar convenis exclusius amb descomptes per als assistents. Torna aviat per veure els codis promocionals!',
+  'accommodation.areas.title': 'Millors Zones',
+  'accommodation.areas.eixample.name': "L'Eixample",
+  'accommodation.areas.eixample.desc':
+    'La zona més segura i elegant, amb arquitectura modernista i a un pas de la seu.',
+  'accommodation.areas.gothic.name': 'Barri Gòtic',
+  'accommodation.areas.gothic.desc':
+    'El centre històric, amb carrers màgics i molta vida nocturna, a 10 min de la UB.',
+  'accommodation.areas.poblenou.name': 'Poblenou / 22@',
+  'accommodation.areas.poblenou.desc':
+    'El hub tecnològic. Un poc més allunyat però molt ben connectat per metro (L1).',
+  'accommodation.apartments.title': 'Apartaments Turístics',
+  'accommodation.apartments.text':
+    "Si prefereixes un apartament, assegura't que compti amb llicència turística oficial (HUTB). Barcelona és molt estricta amb la regulació d'allotjaments.",
+  'accommodation.apartments.link': "Veure guia d'allotjament segur a Barcelona",
+} as const
---
+++ src/i18n/location/index.ts
@@ -0,0 +1,9 @@
+import { es } from './es'
+import { en } from './en'
+import { ca } from './ca'
+
+export const locationTexts = {
+  es,
+  en,
+  ca,
+} as const

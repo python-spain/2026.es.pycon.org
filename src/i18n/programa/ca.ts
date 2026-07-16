776a54320fad058eeeb1f8341bed8d0018500dcd
itziarZG <itziar.zameza.dev@gmail.com>
2026-07-16 13:16:58 +0200

    feat: feat donwload programe button

--- src/i18n/programa/ca.ts
+++ src/i18n/programa/ca.ts
@@ -2,6 +2,7 @@ export const ca = {
   title: 'Programa',
   seoDescription: 'Consulta el cronograma complet de xerrades, tallers i activitats de la PyConES 2026.',
   intro: 'Explora totes les xerrades, tallers i activitats de la PyConES 2026.',
+  downloadPdf: 'Descarregar programa en PDF',
   locale: 'ca',
   noscript: 'JavaScript està desactivat al teu navegador. Per veure el programa sense JavaScript,',
   noscriptLink: 'fes clic aquí',
